6b6882ea8f2e261a21af223467f47dbc4122e6d1
Justin Mason <Justin.Mason@swrve.com>
2022-12-12 11:04:51 +0000

    Remove a noisy log

--- custom_components/solis/service.py
+++ custom_components/solis/service.py
@@ -195,7 +195,6 @@ class InverterService():
                     # the last update
                     try:
                         ts = getattr(data, INVERTER_TIMESTAMP_UPDATE)
-                        _LOGGER.info("timestamp %s", ts)
                         nxt = dt_util.utc_from_timestamp(ts) + update + timedelta(seconds=1)
                         if nxt > dt_util.utcnow():
                             update = nxt - dt_util.utcnow()
